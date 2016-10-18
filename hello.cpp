83fb1dd15a8aba5dcac338f32eff6b0f5bed8fa3
junidev <junigomez1305@gmail.com>
2016-10-18 12:34:33 -0500

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#define constante "Holas Constante"
+
+int main()
+{
+
+// problema 1
+std::cout << "Hola Mundo....  \n";
+// problema 2
+std::cout << constante <<"\n";
+// problema 3
+
+int i =0;
+for(i=0; i<5; i++){
+std::cout << "Hola for  \n";
+}
+
+
+int c1=0;
+while(c1 <= 2)
+{
+	std::cout << "hola while" <<"\n";
+     c1++;
+}
+
+
+int c=0;
+do
+{
+	std::cout << "hola do while" <<"\n";
+     c++;
+}
+while(c<5);
+
+}
+
+
+
+
+
+
