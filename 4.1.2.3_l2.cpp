081cf23bc4f60149b1a722733672e7b1e125391e
junidev <junigomez1305@gmail.com>
2016-10-25 01:34:06 -0500

    Add files via upload

---
+++ 4.1.2.3_l2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int suma(int a,int b){
+    return a+b;
+}
+double sumaDouble(double a,double b){
+    return a+b;
+}
+
+int main(){
+
+cout<<suma(4,5.3)<<endl;
+cout<<sumaDouble(4.34,5.30)<<endl;
+return 0;
+
+}
